6de49779a46f0c40aaf2dd90793ef84e61f6d6bd
Danny <dsmishler@gmail.com>
2026-05-19 13:10:38 -0400

    added bufs to visualization

--- EBA_IR/tools/visualization/output_to_gif.py
+++ EBA_IR/tools/visualization/output_to_gif.py
@@ -1,8 +1,10 @@
 import sys
 import os
 
+w_python = "python3"
+
 if len(sys.argv) < 2:
-    print("usage: python output_to_jpgs.py <target>")
+    print(f"usage: {w_python} output_to_jpgs.py <target>")
     exit(1)
 
 ebaoutput_fname = sys.argv[1]
@@ -12,10 +14,23 @@ if ebaoutput_fname[-7:] != ".ebaout":
     exit(1)
 
 base_fname = ebaoutput_fname[:-7]
+while True:
+    try:
+        base_fname = base_fname[base_fname.index('/')+1:]
+    except ValueError:
+        break
 
-f = open(f"ebaouts/{ebaoutput_fname}")
+f = open(f"{ebaoutput_fname}")
 ftext = f.read()
 f.close()
+
+if os.getcwd() == "/home/danielmishler/Mishler_LL_work/EBA_Work/EBA_models/EBA_IR/tools/visualization":
+    os.system("rm jgrs/*")
+    os.system("rm jpgs/*") # clean up old runs to avoid difficult to track  errors
+else:
+    print(f"warning, this code has specific instructions to be run in:\n")
+    print("/home/danielmishler/Mishler_LL_work/EBA_Work/EBA_models/EBA_IR/tools/visualization\n")
+    print(f"but it is currently being run in {os.getcwd()}\n")
 
 buffer_outputs = ftext.split("CURRENTLY_RUNNING_")[1:]
 # each buffer that outputs something will start with this line
@@ -26,51 +41,71 @@ buffer_outputs = ftext.split("CURRENTLY_RUNNING_")[1:]
 
 imagemagick_convert_str = "convert -size 1080x1080 -delay 80 -loop 0"
 
-displays = {}
-displays["sched"] = {}
-displays["sched"]["occupants"] = None
-displays["sched"]["head"] = None
-displays["sched"]["tail"] = None
-displays["sched"]["name"] = "scheduler"
-displays["read"] = {}
-displays["read"]["occupants"] = None
-displays["read"]["head"] = None
-displays["read"]["tail"] = None
-displays["read"]["name"] = "read queue"
-displays["write"] = {}
-displays["write"]["occupants"] = None
-displays["write"]["head"] = None
-displays["write"]["tail"] = None
-displays["write"]["name"] = "write queue"
+sdisplays = {}
+sdisplays["sched"] = {}
+sdisplays["sched"]["occupants"] = None
+sdisplays["sched"]["head"] = None
+sdisplays["sched"]["tail"] = None
+sdisplays["sched"]["name"] = "scheduler"
+sdisplays["read"] = {}
+sdisplays["read"]["occupants"] = None
+sdisplays["read"]["head"] = None
+sdisplays["read"]["tail"] = None
+sdisplays["read"]["name"] = "read queue"
+sdisplays["write"] = {}
+sdisplays["write"]["occupants"] = None
+sdisplays["write"]["head"] = None
+sdisplays["write"]["tail"] = None
+sdisplays["write"]["name"] = "write queue"
+
+cdisplays = {}
+cdisplays["cbuf"] = {}
+cdisplays["cbuf"]["occupants"] = None
+cdisplays["cbuf"]["head"] = None
+cdisplays["cbuf"]["tail"] = None
+cdisplays["cbuf"]["name"] = "circ buf 1"
 
 arg_bufs_to_names = {}
 arg_bufs_to_names["0xDEADBEEF"] = "empty"
 
-dims = {}
-dims["sched"] = {}
-dims["sched"]["lineheight"] = 5
-dims["sched"]["linewidth"] = 100
-dims["sched"]["start_x"] = -140
-dims["sched"]["start_y"] = -35
-dims["sched"]["num_entries"] = 25
-dims["read"] = {}
-dims["read"]["lineheight"] = 5
-dims["read"]["linewidth"] = 50
-dims["read"]["start_x"] = 0
-dims["read"]["start_y"] = -35
-dims["read"]["num_entries"] = 25
-dims["write"] = {}
-dims["write"]["lineheight"] = 5
-dims["write"]["linewidth"] = 50
-dims["write"]["start_x"] = 90
-dims["write"]["start_y"] = -35
-dims["write"]["num_entries"] = 25
+sdims = {}
+sdims["sched"] = {}
+sdims["sched"]["lineheight"] = 5
+sdims["sched"]["linewidth"] = 100
+sdims["sched"]["start_x"] = -140
+sdims["sched"]["start_y"] = -35
+sdims["sched"]["num_entries"] = 25
+sdims["read"] = {}
+sdims["read"]["lineheight"] = 5
+sdims["read"]["linewidth"] = 50
+sdims["read"]["start_x"] = 0
+sdims["read"]["start_y"] = -35
+sdims["read"]["num_entries"] = 25
+sdims["write"] = {}
+sdims["write"]["lineheight"] = 5
+sdims["write"]["linewidth"] = 50
+sdims["write"]["start_x"] = 90
+sdims["write"]["start_y"] = -35
+sdims["write"]["num_entries"] = 25
+cdims = {}
+cdims["cbuf"] = {}
+cdims["cbuf"]["lineheight"] = 5
+cdims["cbuf"]["linewidth"] = 14
+cdims["cbuf"]["start_x"] = -140
+cdims["cbuf"]["start_y"] = 40
+cdims["cbuf"]["num_xcells"] = 20
+cdims["cbuf"]["num_ycells"] = 10
 
 for i in range(len(buffer_outputs)):
-    jgr_fname = f"{base_fname}_{i}.jgr"
-    jpg_fname = f"{base_fname}_{i}.jpg"
+    sched_jgr_fname = f"sched_{base_fname}_{i}.jgr"
+    sched_jpg_fname = f"sched_{base_fname}_{i}.jpg"
+    buf_jgr_fname = f"buf_{base_fname}_{i}.jgr"
+    buf_jpg_fname = f"buf_{base_fname}_{i}.jpg"
 
-    jgr_f = open(f"jgrs/{jgr_fname}", "w")
+    both_jpg_fname = f"{base_fname}_{i}.jpg"
+
+    sched_jgr_f = open(f"jgrs/{sched_jgr_fname}", "w")
+    buf_jgr_f = open(f"jgrs/{buf_jgr_fname}", "w")
     
     lines = buffer_outputs[i].split('\n')
 
@@ -80,78 +115,150 @@ for i in range(len(buffer_outputs)):
             # print(f"{line} is a translate line")
             arg_bufs_to_names[lines[line_num+1]] = line[10:]
         elif line == "DISPLAY_SCHEDULE_QUEUE":
-            displays["sched"]["head"] = int(lines[line_num+2], 16)/8
-            displays["sched"]["tail"] = int(lines[line_num+3], 16)/8
-            displays["sched"]["occupants"] = lines[line_num+5:line_num+5+dims['sched']['num_entries']]
+            sdisplays["sched"]["head"] = int(lines[line_num+2], base=16)/8
+            sdisplays["sched"]["tail"] = int(lines[line_num+3], base=16)/8
+            sdisplays["sched"]["occupants"] = lines[line_num+5:line_num+5+sdims['sched']['num_entries']]
         elif line == "DISPLAY_READ_QUEUE":
-            displays["read"]["head"] = int(lines[line_num+2], 16)/8
-            displays["read"]["tail"] = int(lines[line_num+3], 16)/8
-            displays["read"]["occupants"] = lines[line_num+5:line_num+5+dims['read']['num_entries']]
+            sdisplays["read"]["head"] = int(lines[line_num+2], base=16)/8
+            sdisplays["read"]["tail"] = int(lines[line_num+3], base=16)/8
+            sdisplays["read"]["occupants"] = lines[line_num+5:line_num+5+sdims['read']['num_entries']]
         elif line == "DISPLAY_WRITE_QUEUE":
-            displays["write"]["head"] = int(lines[line_num+2], 16)/8
-            displays["write"]["tail"] = int(lines[line_num+3], 16)/8
-            displays["write"]["occupants"] = lines[line_num+5:line_num+5+dims['write']['num_entries']]
+            sdisplays["write"]["head"] = int(lines[line_num+2], base=16)/8
+            sdisplays["write"]["tail"] = int(lines[line_num+3], base=16)/8
+            sdisplays["write"]["occupants"] = lines[line_num+5:line_num+5+sdims['write']['num_entries']]
+        elif line == "DISPLAY_CIRC_BUF":
+            cdisplays["cbuf"]["head"] = int(lines[line_num+2], base=16)/8
+            cdisplays["cbuf"]["tail"] = int(lines[line_num+3], base=16)/8
+            start_occ_ln = line_num+5
+            end_occ_ln = line_num+5+cdims['cbuf']['num_xcells']*cdims['cbuf']['num_ycells']
+            cdisplays["cbuf"]["occupants"] = lines[start_occ_ln:end_occ_ln]
 
 
-    jgr_f.write("newgraph\n")
-    jgr_f.write("xaxis min -70 max 70 nodraw\n")
-    jgr_f.write("yaxis min -20 max 20 nodraw\n")
-    jgr_f.write("newstring font Time-Roman fontsize 25 x 0 y 95 hjc vjc :\n")
-    jgr_f.write(f"currently running: {lines[0]}\n")
+    sched_jgr_f.write("newgraph\n")
+    sched_jgr_f.write("xaxis min -70 max 70 nodraw\n")
+    sched_jgr_f.write("yaxis min -20 max 20 nodraw\n")
+    sched_jgr_f.write("newstring font Time-Roman fontsize 25 x 0 y 95 hjc vjc :\n")
+    sched_jgr_f.write(f"currently running: {lines[0]}\n")
 
-    jgr_f.write(f"newstring font Time-Roman fontsize 18 x {-200} y {0} hjc vjc :\n")
-    jgr_f.write("l\n")
-    jgr_f.write(f"newstring font Time-Roman fontsize 18 x {200} y {0} hjc vjc :\n")
-    jgr_f.write("l\n")
-    jgr_f.write(f"newstring font Time-Roman fontsize 18 x {0} y {-60} hjc vjc :\n")
-    jgr_f.write("l\n")
+    sched_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {-200} y {0} hjc vjc :\n")
+    sched_jgr_f.write("l\n")
+    sched_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {200} y {0} hjc vjc :\n")
+    sched_jgr_f.write("l\n")
+    sched_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {0} y {-60} hjc vjc :\n")
+    sched_jgr_f.write("l\n")
 
 
-    for d in dims.values():
-        jgr_f.write("newline poly pfill 1 pts\n")
-        jgr_f.write(f"{d['start_x']} {d['start_y']} {d['start_x']} {d['start_y']+d['num_entries']*d['lineheight']}\n")
-        jgr_f.write("newline poly pfill 1 pts\n")
-        jgr_f.write(f"{d['start_x']+d['linewidth']} {d['start_y']} {d['start_x']+d['linewidth']} {d['start_y']+d['num_entries']*d['lineheight']}\n")
+    buf_jgr_f.write("newgraph\n")
+    buf_jgr_f.write("xaxis min -70 max 70 nodraw\n")
+    buf_jgr_f.write("yaxis min -20 max 20 nodraw\n")
+    buf_jgr_f.write("newstring font Time-Roman fontsize 25 x 0 y 95 hjc vjc :\n")
+    buf_jgr_f.write(f"Status of buffer:\n")
+
+    buf_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {-200} y {0} hjc vjc :\n")
+    buf_jgr_f.write("l\n")
+    buf_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {200} y {0} hjc vjc :\n")
+    buf_jgr_f.write("l\n")
+    buf_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {0} y {-60} hjc vjc :\n")
+    buf_jgr_f.write("l\n")
+
+    # boxes for the sched side
+    for d in sdims.values():
+        sched_jgr_f.write("newline poly pfill 1 pts\n")
+        sched_jgr_f.write(f"{d['start_x']} {d['start_y']} {d['start_x']} {d['start_y']+d['num_entries']*d['lineheight']}\n")
+        sched_jgr_f.write("newline poly pfill 1 pts\n")
+        sched_jgr_f.write(f"{d['start_x']+d['linewidth']} {d['start_y']} {d['start_x']+d['linewidth']} {d['start_y']+d['num_entries']*d['lineheight']}\n")
     
         for entry in range(d['num_entries']+1):
-            jgr_f.write("newline poly pfill 1 pts\n")
-            jgr_f.write(f"{d['start_x']} {d['start_y']+entry*d['lineheight']} {d['start_x']+d['linewidth']} {d['start_y']+entry*d['lineheight']}\n")
+            sched_jgr_f.write("newline poly pfill 1 pts\n")
+            sched_jgr_f.write(f"{d['start_x']} {d['start_y']+entry*d['lineheight']} {d['start_x']+d['linewidth']} {d['start_y']+entry*d['lineheight']}\n")
     
+    # boxes for the cbuf side
+    for d in cdims.values():
+        # for now, it's just the one cbuf. But it'll also help us shorthand
+        for yi in range(d['num_xcells']+1):
+            # drawing all the y-lines
+            buf_jgr_f.write("newline poly pfill 1 pts\n")
+            buf_jgr_f.write(f"{d['start_x']+yi*d['linewidth']} {d['start_y']} {d['start_x']+yi*d['linewidth']} {d['start_y']+d['num_ycells']*d['lineheight']}\n")
+        for xi in range(d['num_ycells']+1):
+            # drawing all the x-lines
+            buf_jgr_f.write("newline poly pfill 1 pts\n")
+            buf_jgr_f.write(f"{d['start_x']} {d['start_y']+xi*d['lineheight']} {d['start_x']+d['num_xcells']*d['linewidth']} {d['start_y']+xi*d['lineheight']}\n")
+        
 
 
-    for key in displays:
-        d = dims[key]
-        t = displays[key]
+
+    # putting in the text for the sched queues
+    for key in sdisplays:
+        d = sdims[key]
+        t = sdisplays[key]
         if t["occupants"] is not None:
             # Assume head and tail are also not none
             assert t["head"] is not None
             assert t["tail"] is not None
 
-            jgr_f.write(f"newstring font Time-Roman fontsize 18 x {d['start_x']-8} y {d['start_y']+d['lineheight']*t['head']+2} hjc vjc :\n")
-            jgr_f.write("h->\n")
-            jgr_f.write(f"newstring font Time-Roman fontsize 18 x {d['start_x']+d['linewidth']+6} y {d['start_y']+d['lineheight']*t['tail']+2} hjc vjc :\n")
-            jgr_f.write("<-t\n")
+            sched_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {d['start_x']-8} y {d['start_y']+d['lineheight']*t['head']+2} hjc vjc :\n")
+            sched_jgr_f.write("h->\n")
+            sched_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {d['start_x']+d['linewidth']+6} y {d['start_y']+d['lineheight']*t['tail']+2} hjc vjc :\n")
+            sched_jgr_f.write("<-t\n")
 
             for occ in range(len(t['occupants'])):
                 if t['occupants'][occ] in arg_bufs_to_names:
                     print_str = arg_bufs_to_names[t['occupants'][occ]]
                 else:
                     print_str = t['occupants'][occ]
-                jgr_f.write(f"newstring font Time-Roman fontsize 15 x {d['start_x']+4} y {d['start_y']+d['lineheight']*occ+2} hjl vjc :\n")
-                jgr_f.write(f"{print_str}\n")
-        jgr_f.write(f"newstring font Time-Roman fontsize 20 x {d['start_x']+d['linewidth']/2} y {d['start_y']-5} hjc vjc :\n")
-        jgr_f.write(f"{t['name']}\n")
+                sched_jgr_f.write(f"newstring font Time-Roman fontsize 15 x {d['start_x']+4} y {d['start_y']+d['lineheight']*occ+2} hjl vjc :\n")
+                sched_jgr_f.write(f"{print_str}\n")
+        sched_jgr_f.write(f"newstring font Time-Roman fontsize 20 x {d['start_x']+d['linewidth']/2} y {d['start_y']-5} hjc vjc :\n")
+        sched_jgr_f.write(f"{t['name']}\n")
+
+    # putting in the contents of the circ buf
+    for key in cdisplays:
+        # should just be cbuf for now
+        d = cdims[key]
+        t = cdisplays[key]
+        if t["occupants"] is not None:
+            # Assume head and tail are also not none
+            assert t["head"] is not None
+            assert t["tail"] is not None
+
+            def to_xcoord(bnum):
+                return (d["start_x"] + (bnum % d["num_xcells"])*d['linewidth'] + d["linewidth"]//2)
+            def to_ycoord(bnum):
+                return (d["start_y"] + (d["num_ycells"]-1)*d["lineheight"] - (bnum // d["num_xcells"])*d['lineheight'] + d["lineheight"]//2)
+
+            # buf_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {to_xcoord(t['head'])} y {to_ycoord(t['head'])} hjc vjc :\n")
+            # buf_jgr_f.write("*h*\n")
+            # buf_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {to_xcoord(t['tail'])} y {to_ycoord(t['tail'])} hjc vjc :\n")
+            # buf_jgr_f.write("*t*\n")
+            buf_jgr_f.write("newline poly color 0.0 1.0 0.0 pfill 1 pts\n")
+            buf_jgr_f.write(f"{-3+to_xcoord(t['head'])+2} {to_ycoord(t['head'])} {-3+to_xcoord(t['head'])} {to_ycoord(t['head'])+2} {-3+to_xcoord(t['head'])-2} {to_ycoord(t['head'])} {-3+to_xcoord(t['head'])} {to_ycoord(t['head'])-2}\n")
+            buf_jgr_f.write("newline poly color 0.0 0.0 1.0 pfill 1 pts\n")
+            buf_jgr_f.write(f"{3+to_xcoord(t['tail'])+2} {to_ycoord(t['tail'])} {3+to_xcoord(t['tail'])} {to_ycoord(t['tail'])+2} {3+to_xcoord(t['tail'])-2} {to_ycoord(t['tail'])} {3+to_xcoord(t['tail'])} {to_ycoord(t['tail'])-2}\n")
+
+            for occ in range(len(t['occupants'])):
+                text = t['occupants'][occ]
+                byte = text.split()[-1]
+                char = chr(int(byte, base=16))
+
+                buf_jgr_f.write(f"newstring font Time-Roman fontsize 18 x {to_xcoord(occ)} y {to_ycoord(occ)} hjc vjc :\n")
+                buf_jgr_f.write(f"{char}\n")
+        buf_jgr_f.write(f"newstring font Time-Roman fontsize 20 x {d['start_x']+(d['linewidth']*d['num_xcells'])//2} y {d['start_y']-5} hjc vjc :\n")
+        buf_jgr_f.write(f"{t['name']}\n")
 
 
 
-    jgr_f.close()
 
-    os.system(f"python3 jgr_to_jpg.py {jgr_fname}")
-    print(f"written jpgs/{jpg_fname} ({i+1} of {len(buffer_outputs)})")
+    sched_jgr_f.close()
+    buf_jgr_f.close()
+
+    os.system(f"{w_python} jgr_to_jpg.py {sched_jgr_fname}")
+    os.system(f"{w_python} jgr_to_jpg.py {buf_jgr_fname}")
+    print(f"written jpgs/{sched_jpg_fname}, jpgs/{buf_jpg_fname} ({i+1} of {len(buffer_outputs)})")
     # adding each individually instead of all at once with "*". This
     # ensures we only use the ones we parsed and not some other stuff
     # sitting around from a different run
-    imagemagick_convert_str += f" jpgs/{jpg_fname}"
+    os.system(f"convert jpgs/{sched_jpg_fname} jpgs/{buf_jpg_fname} +append jpgs/{both_jpg_fname}")
+    imagemagick_convert_str += f" jpgs/{both_jpg_fname}"
 
 imagemagick_convert_str += f" gifs/{base_fname}.gif"
 
