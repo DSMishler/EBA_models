0ae4b0bdea3ec0feb03c8811f593db20d4427453
Danny <dsmishler@gmail.com>
2026-06-01 16:35:49 -0400

    adding alloc remote

--- EBA_IR/interpreter.c
+++ EBA_IR/interpreter.c
@@ -163,6 +163,28 @@ void run_bufreq(IR_state_t *IRstate, char **line)
 
       buf_free_if_shorthand(allocation_len_buf, line[3]);
    }
+   else if (match_second_word(line, "ALLOC_REMOTE"))
+   {
+      void *remote_buf_id_buf = parse_var_buf(line[2], IRstate);
+      int64_t remote_buf_id = *((int64_t *)remote_buf_id_buf);
+      remote_buf_id += 0; // keep compilers happy while it's unused
+
+      int var_dest = parse_variable(line[3]);
+      if (var_dest < 0 || var_dest >= IR_STATE_SIZE)
+      {
+         var_errmsg("BUFREQ ALLOC_REMOTE", IRstate->next_line);
+      }
+
+      void *allocation_len_buf = parse_var_buf(line[4], IRstate);
+      int64_t allocation_len = *((int64_t *)allocation_len_buf);
+
+      void* newbuf = malloc(allocation_len);
+      // printf("buf alloc-ed: 0x%lx\n", (uint64_t)newbuf);
+      IRstate->vars[var_dest] = (int64_t) newbuf;
+
+      buf_free_if_shorthand(remote_buf_id_buf, line[2]);
+      buf_free_if_shorthand(allocation_len_buf, line[4]);
+   }
    else if (match_second_word(line, "ALLOC_LITERAL"))
    {
       // alloc_literal should NOT be deprecated because it would be
