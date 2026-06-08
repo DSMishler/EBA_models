fc8f4adbb1a671a5ef2bae50ef7fb856bcbfdc1a
Danny <dsmishler@gmail.com>
2026-06-08 15:22:43 -0400

    finished gl test and moving to scaffolding for EIR demo

--- EBA_IR/glfw_testing/gltest.c
+++ EBA_IR/glfw_testing/gltest.c
@@ -40,28 +40,45 @@ int main( void ) {
    printf( "OpenGL version supported %s.\n", glGetString( GL_VERSION ) );
    
    /* OTHER STUFF GOES HERE NEXT */
-   float tri_points[] =
+   double tri_points[] =
    {
       +0.0f, +0.9f, +0.0f,
       +0.5f, -0.5f, +0.0f,
-      -0.5f, -0.5f, +0.0f
+      -0.5f, -0.5f, +0.0f,
+      -0.5f, -0.9f, +0.0f
+      -0.5f, -0.8f, +0.0f,
+      -0.9f, -0.7f, +0.0f,
    };
 
    // vertex buffer object
-   GLuint vbo = 0;
-   glGenBuffers(1, &vbo);
-   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-   glBufferData(GL_ARRAY_BUFFER, 9*sizeof(float), tri_points, GL_STATIC_DRAW);
+   GLuint vbo1 = 0;
+   glGenBuffers(1, &vbo1);
+   glBindBuffer(GL_ARRAY_BUFFER, vbo1);
+   glBufferData(GL_ARRAY_BUFFER, 18*sizeof(double), tri_points, GL_STATIC_DRAW);
 
    // vertex array object (array of vertex buffer objects (even just one!))
-   GLuint vao = 0;
-   glGenVertexArrays(1, &vao);
-   glBindVertexArray(vao);
+   GLuint vao1 = 0;
+   glGenVertexArrays(1, &vao1);
+   glBindVertexArray(vao1);
    glEnableVertexAttribArray(0);
-   glBindBuffer(GL_ARRAY_BUFFER, vbo); // why not vao here?
+   glBindBuffer(GL_ARRAY_BUFFER, vbo1); // why not vao1 here?
    // not sure if that above line is needed either.
    // can remove it and things work fine!
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+   glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 0, NULL);
+
+   // second vertex buffer obj!
+   GLuint vbo2 = 0;
+   glGenBuffers(1, &vbo2);
+   glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+   glBufferData(GL_ARRAY_BUFFER, 18*sizeof(double), tri_points, GL_STATIC_DRAW);
+
+   // and vertex array
+   GLuint vao2 = 0;
+   glGenVertexArrays(1, &vao2);
+   glBindVertexArray(vao2);
+   glEnableVertexAttribArray(0);
+   glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+   glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 0, NULL);
 
    
    int chars_read;
@@ -79,67 +96,80 @@ int main( void ) {
    fclose(vf);
    vertex_shader[vfsize] = '\0';
 
-   void is_r_init(char *s)
-   {
-      int i;
-      for(i = 0; s[i] != 0; i++)
-      {
-         printf("char %d: %c (%d)\n", i, s[i], s[i]);
-         if (s[i] == '\r')
-         {
-            printf("\\r is indeed in the string\n");
-         }
-      }
-   }
-
-   FILE *ff = fopen("gltest.frag", "r");
+   FILE *ff = fopen("gltest1.frag", "rb");
    fseek(ff, 0, SEEK_END);
    long int ffsize = ftell(ff);
    fseek(ff, 0, SEEK_SET);
-   char *fragment_shader = malloc(ffsize+1);
-   chars_read = fread(fragment_shader, 1, ffsize, ff);
+   char *fragment_shader1 = malloc(ffsize+1);
+   chars_read = fread(fragment_shader1, 1, ffsize, ff);
    if(chars_read != ffsize)
    {
       printf("error in reading fragment shader!\n");
       exit(1);
    }
    fclose(ff);
-   fragment_shader[ffsize] = '\0';
+   fragment_shader1[ffsize] = '\0';
 
-   is_r_init(vertex_shader);
-   printf("now fragment:\n");
-   is_r_init(fragment_shader);
+   ff = fopen("gltest2.frag", "rb");
+   fseek(ff, 0, SEEK_END);
+   ffsize = ftell(ff);
+   fseek(ff, 0, SEEK_SET);
+   char *fragment_shader2 = malloc(ffsize+1);
+   chars_read = fread(fragment_shader2, 1, ffsize, ff);
+   if(chars_read != ffsize)
+   {
+      printf("error in reading fragment shader!\n");
+      exit(1);
+   }
+   fclose(ff);
+   fragment_shader2[ffsize] = '\0';
 
    
    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
    glShaderSource(vs, 1, (const char**) &vertex_shader, NULL);
    glCompileShader(vs);
 
-   GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-   glShaderSource(fs, 1, (const char**) &fragment_shader, NULL);
-   glCompileShader(fs);
+   GLuint fs1 = glCreateShader(GL_FRAGMENT_SHADER);
+   glShaderSource(fs1, 1, (const char**) &fragment_shader1, NULL);
+   glCompileShader(fs1);
 
-   GLuint shader_program = glCreateProgram();
-   glAttachShader(shader_program, fs);
-   glAttachShader(shader_program, vs);
-   glLinkProgram(shader_program);
+   GLuint fs2 = glCreateShader(GL_FRAGMENT_SHADER);
+   glShaderSource(fs2, 1, (const char**) &fragment_shader2, NULL);
+   glCompileShader(fs2);
+
+   GLuint shader_program1 = glCreateProgram();
+   glAttachShader(shader_program1, fs1);
+   glAttachShader(shader_program1, vs);
+   glLinkProgram(shader_program1);
+
+   GLuint shader_program2 = glCreateProgram();
+   glAttachShader(shader_program2, fs2);
+   glAttachShader(shader_program2, vs);
+   glLinkProgram(shader_program2);
+   glClearColor(0.6f, 0.6f, 0.8f, 1.0f);
 
    while(!glfwWindowShouldClose(window))
    {
       glfwPollEvents();
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-      glUseProgram(shader_program);
-      glBindVertexArray(vao);
+      glUseProgram(shader_program1);
+      glBindVertexArray(vao1);
 
-      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glDrawArrays(GL_TRIANGLES, 0, 6);
+
+      glUseProgram(shader_program2);
+      glBindVertexArray(vao2);
+      glDrawArrays(GL_TRIANGLES, 1, 3);
+      // glDrawArrays(GL_LINES, 0, 6);
+      // glDrawArrays(GL_POINTS, 0, 6);
 
       glfwSwapBuffers(window);
    }
      
    // Close OpenGL window, context, and any other GLFW resources.
    free(vertex_shader);
-   free(fragment_shader);
+   free(fragment_shader1);
    glfwTerminate();
    return 0;
 }
