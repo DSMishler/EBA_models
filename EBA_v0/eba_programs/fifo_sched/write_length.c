2dfaaa774c88244bf8987a6ace461f5669e3254d
Danny <dsmishler@gmail.com>
2026-09-25 10:27:31 -0400

    beginning of write_length

---
+++ EBA_v0/eba_programs/fifo_sched/write_length.c
@@ -0,0 +1,21 @@
+
+
+
+// takes a known FIFO scheduler and writes out its length to a file
+void write_length(void *args)
+{
+   // args are:
+   // 0: this operation
+   // 1: the scheduler
+   // 2: the file pointer of the file to write to
+
+
+   int length = 11;
+   // MISSING: calculate the length of the FIFO scheduler's queue
+
+
+
+
+
+   // put myself back in the queue if there is more work to be done
+}
--- EBA_v0/eba_programs/sched_demo.c
+++ EBA_v0/eba_programs/sched_demo.c
@@ -8,6 +8,8 @@ void prog_entry(void* args)
 {
    free_eba_arg(args);
    printf("scheduler demo\n");
+   // SCHEDULER INIT
+
    op_loader_t *fs_main_opl = opl_init("eba_programs/fifo_sched/fifo_sched.so", "sched_run");
    op_loader_t *fs_init_opl = opl_init("eba_programs/fifo_sched/fifo_sched.so", "sched_init");
 
@@ -22,8 +24,12 @@ void prog_entry(void* args)
    fifo_sched_t *fs = get_eba_arg(fs_init_arg, 1);
    set_eba_arg(fs_main_arg, 1, fs);
 
+
+
+   // RUN
    eba_op(fs_main_arg);
 
+   // CLEANUP
    opl_destroy(fs_main_opl);
    opl_destroy(fs_init_opl);
    free_eba_arg(fs_main_arg);
